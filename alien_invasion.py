de29796593931aa397eb6399fca23e07dd9a56f2
Neah Garza <Neah_Garza2@baylor.edu>
2021-03-07 00:06:37 -0600

    Allowed for continuous movement when the right key is pressed

--- alien_invasion.py
+++ alien_invasion.py
@@ -36,18 +36,23 @@ class AlienInvasion:
         """Start the main loop for the game."""
         while True:
             self._check_events()
+            self.ship.update()
             self._update_screen()
 
     def _check_events(self):
         #Respond to keyboard and mouse events. 
         for event in pygame.event.get():
+            #if exit
             if event.type == pygame.QUIT:
                 sys.exit()
-
+            #if right key is pressed
             elif event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_RIGHT:
-                    #Move the ship to the right
-                    self.ship.rect.x += 1
+                    self.ship.moving_right = True
+            #if right key was let go
+            elif event.type == pygame.KEYUP:
+                if event.key == pygame.K_RIGHT:
+                    self.ship.moving_right = False
     
     def _update_screen(self):
         """Updates images on the screen, and flip to the new screen"""
