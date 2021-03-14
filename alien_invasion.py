47820652587e08dff851b5a516cea95b94e63df4
Neah Garza <Neah_Garza2@baylor.edu>
2021-03-14 03:06:17 -0500

    Updated play button

--- alien_invasion.py
+++ alien_invasion.py
@@ -85,20 +85,22 @@ class AlienInvasion:
     
     def _check_play_button(self, mouse_pos):
         """Start a new game when the Player clicks Play"""
-        #Next line checks whether mouse click overlaps the Play button region; 
-        #   if yes, set game_active to True and start game
-        if self.play_button.rect.collidepoint(mouse_pos):
-            #Reset the game statistics
-            self.stats.reset_stats()
-            self.stats.game_active = True
+        button_clicked = self.play_button.rect.collidepoint(mouse_pos)
+        if button_clicked and not self.stats.game_active:
+            #Next line checks whether mouse click overlaps the Play button region; 
+            #   if yes, set game_active to True and start game
+            if self.play_button.rect.collidepoint(mouse_pos):
+                #Reset the game statistics
+                self.stats.reset_stats()
+                self.stats.game_active = True
 
-            #Get rid of any remaining aliens and bullets
-            self.aliens.empty()
-            self.bullets.empty()
+                #Get rid of any remaining aliens and bullets
+                self.aliens.empty()
+                self.bullets.empty()
 
-            #Create a new fleet and center the ship
-            self._create_fleet()
-            self.ship.center_ship()
+                #Create a new fleet and center the ship
+                self._create_fleet()
+                self.ship.center_ship()
 
     def _check_keydown_events(self, event):
         """Respond to key presses"""
