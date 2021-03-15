894bab5e792a643001bbca0e4e9f8180e9ca7585
Neah Garza <Neah_Garza2@baylor.edu>
2021-03-14 23:45:50 -0500

    Added high scores

--- game_stats.py
+++ game_stats.py
@@ -8,6 +8,8 @@ class GameStats:
         self.reset_stats()
         #Start Alien Invasion in an active state (Player has lives left)
         self.game_active = False
+        #High score should never be reset (that's why it's in init instead of reset_stats)
+        self.high_score = 0
 
     def reset_stats(self):
         """Initialize statistics that can change during the game"""
